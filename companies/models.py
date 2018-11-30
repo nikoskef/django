488d886a0f7f08a79f4f1d6ea56704066231e472
nikoskef <nikoskef86@gmail.com>
2018-11-30 18:51:58 +0200

    fix templates and migrations

--- companies/models.py
+++ companies/models.py
@@ -7,6 +7,7 @@ class Company(models.Model):
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
+    website = models.CharField(max_length=200, default='www')
     is_mvp = models.BooleanField(default=False)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=20)
